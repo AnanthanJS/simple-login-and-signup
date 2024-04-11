6b78d8bb9dbf32aa803ec432e3d78e6795bda646
Ananthakrishnan J S <157231676+AnanthanJS@users.noreply.github.com>
2024-04-11 16:17:31 +0530

    Delete login_project directory

--- login_project/login_project/info.py
+++
@@ -1,6 +0,0 @@
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ananthakrishnanjs816@gmail.com'
-EMAIL_HOST_PASSWORD = 'pgwsvhwwgqispldf'
-EMAIL_PORT = 587
